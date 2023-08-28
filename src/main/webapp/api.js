7aef9df120500ae29c364d84be2541117c51e6bb
vixxjy <ajorvictor48@gmail.com>
2023-08-28 15:23:07 +0100

    [database] database backup updates

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,11 +1,8 @@
-
-
 export const token = new URLSearchParams(window.location.search).get("jwt");
 export const url = "/api/v1/";
-
 /*
 export const url = "http://localhost:8383/api/v1/";
 export const apiUrl = "http://localhost:9090/api/";
 export const token =
-  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjkyNjcyMjAzfQ.7Yh2xoPn0bLQCDJ6S48b2pd2qXWlP2oihHwkAeVKE0Zcm6UCtE0Of9TpzpLm5m9Liv_koIJm0x732nxfj8wIpA";
+  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjkzMjc1MTI2fQ.kOKX-tgw6Ld77mfVB6wnvdnecSXsKB_uJK4WANgyYVKKNvhFDxhcdxTeY0JzcCjs8UdHwtdD193uusIRgoGmLQ";
 */
