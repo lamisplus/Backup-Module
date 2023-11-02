fab4d67714cf90e7a5c169d1c3bdf1c33da44357
tecteni <yakubganiyat96@gmail.com>
2023-11-02 01:46:28 -0700

    added a message loader button for easy Ux

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,4 +1,4 @@
-export const token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjk4ODU3MzUyfQ.2U_B6r_s-BveM0VM7byzq-B-JNQ6R8Yxw_-Ogug-siMCvuKYaCll7k-ul6mMK8o-bHL82m9zxWuADQ_m32cCgw'
+export const token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjk4OTQyOTM4fQ.SIFsA9EUQOVuSOAFct2WLq33giyWlH0UIZJVNdobj-J5Ha8rJMg6wBLKy_yianWq5qHT_tN83N20AooX3tzK2w'
 export const url = "http://localhost:8383/api/v1/";
 /*
 export const url = "http://localhost:8383/api/v1/";
