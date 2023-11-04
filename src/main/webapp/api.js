a7185d08bffeedab57c2205002c2d857e83d7cb3
Mathew Adegbite <mathewadegbite@gmail.com>
2023-11-04 12:25:23 +0100

    Adding deynamic base url to api.js

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,8 +1,6 @@
-export const token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjk4OTQyOTM4fQ.SIFsA9EUQOVuSOAFct2WLq33giyWlH0UIZJVNdobj-J5Ha8rJMg6wBLKy_yianWq5qHT_tN83N20AooX3tzK2w'
-export const url = "http://localhost:8383/api/v1/";
-/*
-export const url = "http://localhost:8383/api/v1/";
-export const apiUrl = "http://localhost:9090/api/";
-export const token =
-  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjkzMjc1MTI2fQ.kOKX-tgw6Ld77mfVB6wnvdnecSXsKB_uJK4WANgyYVKKNvhFDxhcdxTeY0JzcCjs8UdHwtdD193uusIRgoGmLQ";
-*/
+export const token = new URLSearchParams(window.location.search).get("jwt");
+export const url = "/api/v1/";
+
+// export const token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjk4OTQyOTM4fQ.SIFsA9EUQOVuSOAFct2WLq33giyWlH0UIZJVNdobj-J5Ha8rJMg6wBLKy_yianWq5qHT_tN83N20AooX3tzK2w'
+// export const url = "http://localhost:8383/api/v1/";
+//
